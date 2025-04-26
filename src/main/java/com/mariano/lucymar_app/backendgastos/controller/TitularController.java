7c1ff241000324c1d6cf3931d9bfd7947e31aa63
maguero73 <maguerotecnico@gmail.com>
2025-04-26 15:37:29 -0300

    Arreglo_cors_titulares_springboot_26-04-2025

--- src/main/java/com/mariano/lucymar_app/backendgastos/controller/TitularController.java
+++ src/main/java/com/mariano/lucymar_app/backendgastos/controller/TitularController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api/titulares")
 public class TitularController {
